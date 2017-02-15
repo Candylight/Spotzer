7cf7609692835087528d1b4ad8f665f925011f1b
sgourier <sylvain.gourier@macroprog.fr>
2017-02-15 22:00:26 +0100

    Add api services files and config

---
+++ src/AppBundle/Services/SpotifyFunctions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Sylvain Gourier
+ * Date: 15/02/2017
+ * Time: 19:10
+ */
+
+namespace AppBundle\Services;
+
+class SpotifyFunctions
+{
+
+}
---
+++ src/AppBundle/Services/YoutubeFunctions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sylvain Gourier
+ * Date: 15/02/2017
+ * Time: 19:11
+ */
+
+namespace AppBundle\Services;
+
+
+class YoutubeFunctions
+{
+
+}
