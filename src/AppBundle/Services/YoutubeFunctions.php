ff8a2869f9f4ae24b0b98ec6315e4fa9fe4047ef
behnoushnorouzi <behnoush.norouzi@yahoo.com>
2017-04-06 13:43:17 +0200

    youtube oauth connection and callback

--- src/AppBundle/Services/YoutubeFunctions.php
+++ src/AppBundle/Services/YoutubeFunctions.php
@@ -9,7 +9,51 @@
 namespace AppBundle\Services;
 
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+
 class YoutubeFunctions
 {
+    private $youtubeClientSecret;
+    private $youtubeClientId;
+    private $client;
+    private $code;
+
+    /**
+     * YoutubeFunctions constructor.
+     */
+    public function __construct($youtubeClientId, $youtubeClientSecret)
+    {
+        $this->youtubeClientId = $youtubeClientId;
+        $this->youtubeClientSecret = $youtubeClientSecret;
+
+        $this->client = new \Google_Client();
+
+    }
+
+    public function getAuthorizationUrl($redirectUrl){
+
+
+        $this->client->setApplicationName('Spotzer');
+        $this->client->setClientId($this->youtubeClientId);
+        $this->client->setClientSecret($this->youtubeClientSecret);
+        $this->client->setAccessType("offline");    //offline access
+        $this->client->setIncludeGrantedScopes(true);  //incremental auth
+        $this->client->addScope(\Google_Service_YouTube::YOUTUBE_FORCE_SSL);
+        $this->client->setRedirectUri($redirectUrl);
+
+        return  $this->client->createAuthUrl();
+
+    }
+    
+    public function getToken($code){
+
+        $this->client->authenticate($code);
+
+        return $this->client->getAccessToken();
+
+    }
+
+
 
 }
