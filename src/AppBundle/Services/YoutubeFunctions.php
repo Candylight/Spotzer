599b1002362f81f913e914430bda8667dd00d1f7
Behnoush <behnoush.norouzi@yahoo.com>
2017-04-14 15:18:41 +0200

    Remove duplicate data

--- src/AppBundle/Services/YoutubeFunctions.php
+++ src/AppBundle/Services/YoutubeFunctions.php
@@ -16,58 +16,6 @@ class YoutubeFunctions
 {
     private $youtubeClientSecret;
     private $youtubeClientId;
-    private $client;
-    private $code;
-
-    /**
-     * YoutubeFunctions constructor.
-     * @param $youtubeClientId
-     * @param $youtubeClientSecret
-     */
-    public function __construct($youtubeClientId, $youtubeClientSecret)
-    {
-        $this->youtubeClientId = $youtubeClientId;
-        $this->youtubeClientSecret = $youtubeClientSecret;
-
-        $this->client = new \Google_Client();
-
-    }
-
-    /**
-     * @param $redirectUrl
-     * @return string  HttpResponse
-     */
-    public function getAuthorizationUrl($redirectUrl){
-
-
-        $this->client->setApplicationName('Spotzer');
-        $this->client->setClientId($this->youtubeClientId);
-        $this->client->setClientSecret($this->youtubeClientSecret);
-        $this->client->setAccessType("offline");    //offline access
-        $this->client->setIncludeGrantedScopes(true);  //incremental auth
-        $this->client->addScope(\Google_Service_YouTube::YOUTUBE_FORCE_SSL);
-        $this->client->setRedirectUri($redirectUrl);
-
-        return  $this->client->createAuthUrl();
-
-    }
-
-    /**
-     * @param $code
-     * @return array Token
-     */
-    public function getToken($code){
-
-        $this->client->authenticate($code);
-
-        return $this->client->getAccessToken();
-
-    }
-
-
-
-    private $youtubeClientSecret;
-    private $youtubeClientId;
     private $youtubeApiKey;
     private $client;
     private $youtube;
