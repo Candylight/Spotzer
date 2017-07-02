dcd83b641b81e2eaca691a5f03374b82a6c707eb
Sylvai Madame Gourie <sylvain.gourier@macroprog.fr>
2017-07-02 21:32:03 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -31,4 +31,19 @@ class DefaultController extends Controller
             return $this->redirectToRoute('homepage');
         }
     }
+
+    /**
+     * @Route("/", name="default")
+     */
+    public function defaultAction()
+    {
+        if($this->getUser() != null)
+        {
+            return $this->redirectToRoute('dashboard');
+        }
+        else
+        {
+            return $this->redirectToRoute('homepage');
+        }
+    }
 }
