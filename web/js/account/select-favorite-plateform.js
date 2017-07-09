6e98015ae045b732d6c7e5602355d7c09231b545
Sylvain Gourier <sylvain.gourier@macroprog.fr>
2017-07-09 15:51:01 +0200

    Fix: account modification prefered platform option

--- web/js/account/select-favorite-plateform.js
+++ web/js/account/select-favorite-plateform.js
@@ -1,7 +1,9 @@
-$(".plateform-choice").click( function(){
-    $(".plateform-choice").removeClass("active");
-    $(this).addClass("active");
+$(document).ready(function () {
+    $(".plateform-choice").on("click", function(){
+        $(".plateform-choice").removeClass("active");
+        $(this).addClass("active");
 
-    var value = $(this).data("value");
-    $("#appbundle_user_preferedPlatform").val(value);
+        var value = $(this).data("value");
+        $("#appbundle_user_preferedPlatform").val(value);
+    });
 });
