276d3fd979b410cf90f012ddb9432307aa0105e9
Thibault <th.jullion@gmail.com>
2017-07-10 14:58:08 +0200

    [INTEGRATION] Popin : Mise en forme de la popin youtube et des intéractions

--- web/js/overlay/popinyoutube.js
+++ web/js/overlay/popinyoutube.js
@@ -3,13 +3,26 @@ var url_embed_youtube = "https://www.youtube.com/watch?v=";
 
 
 $(".video-link-popin").click( function(){
+    var container_video = document.getElementById("overlay-video");
+
+    container_video.innerHTML  = "";
+
     var overlay = $(".overlay-youtube");
     if( overlay.hasClass("active") ){
         overlay.removeClass("active");
     }
     else{
         $id_video = $(this).data("link");
-        $("#video-popin").attr("src",url_embed_youtube+$id_video);
+        var iframe = document.createElement( "iframe" );
+
+        iframe.setAttribute( "frameborder", "0" );
+        iframe.setAttribute( "allowfullscreen", "" );
+        iframe.setAttribute( "height", "315" );
+        iframe.setAttribute( "width", "420" );
+        iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ $id_video +"?rel=0&showinfo=0&autoplay=1" );
+
+        container_video.appendChild( iframe );
+
         overlay.addClass("active");
     }
 });
