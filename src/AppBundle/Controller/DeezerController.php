0a1d3ef62b83a6223d5157a44279bf6cb9a4b1f6
sgourier <sylvain.gourier@macroprog.com>
2017-07-11 15:47:12 +0200

    Fix: correct use in deezerController

--- src/AppBundle/Controller/DeezerController.php
+++ src/AppBundle/Controller/DeezerController.php
@@ -10,8 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\HttpFoundation\Request;
-
 
 /**
  * Class
