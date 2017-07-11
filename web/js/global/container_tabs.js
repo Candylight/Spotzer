d9092748ac612f15c3bf5eaa7d741556420c2aa8
Thibault <th.jullion@gmail.com>
2017-07-12 00:44:21 +0200

    [INTEGRATION] Thumbnails : Suppression librairie Remodel inutilisée (2)

---
+++ web/js/global/container_tabs.js
@@ -0,0 +1,11 @@
+$(".container-tab > .thumbnails > .item").click( function(){
+    var nb = $(this).data("thumb");
+
+    // Desactivation des onglets et activation de l'onglet cliqué
+    $(".container-tab > .thumbnails > .item").removeClass("active");
+    $(this).addClass("active");
+
+    // Désactivation des contenu et activation du contenu de l'onglet cliqué
+    $(".content-thumb").removeClass("active");
+    $(".thumb-"+nb).addClass("active");
+});
