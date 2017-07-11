60d05abd0ebdbde59f13129b4532cbbb617f8560
Thibault <th.jullion@gmail.com>
2017-07-11 18:45:52 +0200

    [INTEGRATION] AddPlaylist : Mise en place du formulaire

--- web/js/global/addToPlaylist.js
+++ web/js/global/addToPlaylist.js
@@ -1,12 +1,23 @@
 
-$(".add-to-playlist").click( function(){
-    var type = $(this).data("type");
-    var idObject = $(this).data("idsong");
+$(document).ready(function(){
+    $(".add-to-playlist").click( function(){
+
+        if( $(".overlay-add-playlist").hasClass("active")){
+            $(".overlay-add-playlist").removeClass("active");
+        }
+        else{
+            var type = $(this).data("type");
+            var name = $(this).data("name");
+            var idToAdd = $(this).data("id");
 
 
-    $(".add-form").addClass("active");
-    $("."+type+"-add-form").addClass("active");
+            $(".add-form").removeClass("active");
+            $("."+type+"-add-form").addClass("active");
 
+            $(".name-add-to").html(name);
+            $(".id-track").val(idToAdd);
 
-    $(".overlay-add-playlist").addClass("active");
+            $(".overlay-add-playlist").addClass("active");
+        }
+    });
 });
